df100fff6cf22005d1a039bbf9e7e97434a6fdf6
Stephen Chow <stephenchow2k@gmail.com>
2018-07-05 13:08:51 -0400

    Initial commit

--- game/index.js
+++ game/index.js
@@ -1,6 +1,21 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+export default function reducer(
+  state = { board: new Map(), turn: 'X' },
+  action
+) {
+  switch (action.type) {
+    case 'MOVE':
+      let newBoard = state.board.setIn(action.position, action.turn);
+      // do we need action.turn?
+      return state.turn === 'X'
+        ? { board: newBoard, turn: 'O' }
+        : { board: newBoard, turn: 'X' };
+    default:
+      return state;
+  }
 }
+
+export const move = (turn, pos) => {
+  return { type: 'MOVE', position: [pos[0], pos[1]], turn };
+};
