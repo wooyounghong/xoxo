d81ca4576f5456c56553f75ccbe3dcf105afd190
Stephen Chow <stephenchow2k@gmail.com>
2018-07-05 15:48:17 -0400

    Finished Winner

--- game/index.js
+++ game/index.js
@@ -9,7 +9,7 @@ export default function reducer(
       let newBoard = state.board.setIn(action.position, action.turn);
       // do we need action.turn?
       let winnerFunc = winner(newBoard);
-      if (winnerFunc === 'not finished yet') console.log(winnerFunc);
+      console.log(winnerFunc);
       return state.turn === 'X'
         ? { board: newBoard, turn: 'O', winner: winnerFunc }
         : { board: newBoard, turn: 'X', winner: winnerFunc };
@@ -18,33 +18,57 @@ export default function reducer(
   }
 }
 
-
-
 export const move = (turn, pos) => {
   return { type: 'MOVE', position: pos, turn };
 };
 
 export const streak = (board, firstCoord, ...remainingCoords) => {
   // remainingCoords = [[1, 1], [2,2]]
-  const value1 = board[firstCoord[0]][firstCoord[1]];
-  console.log(value1);
-  const triple = remainingCords.every((coords) => {
-    return board[coords[0]][coords[1]] === value1
-  })
+  // if (!board[firstCoord[0]] || !board[firstCoord[0]][firstCoord[1]]) {
+  //   console.log(board);
+  //   console.log(firstCoord);
+  //   console.log(typeof firstCoord[0]);
+  //   console.log(board.getIn(firstCoord));
+  //   console.log('first');
+  //   return undefined;
+  // }
+  const value1 = board.getIn(firstCoord);
+  const triple = remainingCoords.every(coords => {
+    return board.getIn(coords) === value1;
+  });
   if (triple) return value1;
-  return undefined
+  return undefined;
 };
 
-
-export const winner = (board) => {
-  const win = ''
-  return streak(board, [0, 0], [0, 1], [0, 2]) || streak(board, [1, 0], [1, 1], [1, 2]) || streak(board, [2, 0], [2, 1], [2, 2]) || streak(board, [0, 0], [1, 0], [2, 0]) || streak(board, [0, 1], [1, 1], [2, 1]) || streak(board, [0, 2], [1, 2], [2, 2]) || streak(board, [0, 0], [1, 1], [2, 2]) || streak(board, [2, 0], [1, 1], [0, 2]) || checkEmpty(board);
-}
+export const winner = board => {
+  const win = '';
+  return (
+    streak(board, [0, 0], [0, 1], [0, 2]) ||
+    streak(board, [1, 0], [1, 1], [1, 2]) ||
+    streak(board, [2, 0], [2, 1], [2, 2]) ||
+    streak(board, [0, 0], [1, 0], [2, 0]) ||
+    streak(board, [0, 1], [1, 1], [2, 1]) ||
+    streak(board, [0, 2], [1, 2], [2, 2]) ||
+    streak(board, [0, 0], [1, 1], [2, 2]) ||
+    streak(board, [2, 0], [1, 1], [0, 2]) ||
+    checkEmpty(board)
+  );
+};
 
 // checker function if no winner found yet;
-const checkEmpty = (board) => {
-  if (board[0][0] === '_' || board[0][1] === '_' || board[0][2] === '_' || board[1][0] === '_' || board[1][1] === '_' || board[1][2] === '_' || board[2][0] === '_' || board[2][1] === '_' || board[2][2] === '_') {
+const checkEmpty = board => {
+  if (
+    !board.hasIn([0, 0]) ||
+    !board.hasIn([0, 1]) ||
+    !board.hasIn([0, 2]) ||
+    !board.hasIn([1, 0]) ||
+    !board.hasIn([1, 1]) ||
+    !board.hasIn([1, 2]) ||
+    !board.hasIn([2, 0]) ||
+    !board.hasIn([2, 1]) ||
+    !board.hasIn([2, 2])
+  ) {
     return 'not finished yet';
   }
   return 'draw';
-}
+};
